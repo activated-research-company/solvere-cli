28a9f1e8fecec8ce2da9fd597228341e5d950f40
Adam Lee <adam.lee@activatedresearch.com>
2020-04-30 13:51:36 -0500

    default advice to green

--- src/advisor/advisor.ts
+++ src/advisor/advisor.ts
@@ -12,11 +12,11 @@ class Advisor {
   public connect(observable: Observable<Answers>) {
     observable.subscribe(this.onQuestionAnswered.bind(this));
   }
-  
+
   public advise(message?: string, color?: chalk.Chalk): Advisor {
     if (message) {
       if (!this.adviseGiven) { console.log(''); }
-      console.log(color ? color(message) : message);
+      console.log(color ? color(message) : chalk.green(message));
       if (!this.adviseGiven) { console.log(''); }
       this.adviseGiven = true;
     }
